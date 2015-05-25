9fd36bedab152581945909446bb2d263248ab6c6
alanxuefei <alan.xuefei@googlemail.com>
2015-05-25 14:52:23 +0800

    The Google Activity Recognition Service Works

--- app/src/main/java/com/example/alan/liveable_city_beta/MainActivity.java
+++ app/src/main/java/com/example/alan/liveable_city_beta/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.alan.liveable_city_beta;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
--- app/src/main/java/com/example/alan/liveable_city_beta/SensorListenerService.java
+++ app/src/main/java/com/example/alan/liveable_city_beta/SensorListenerService.java
@@ -1,30 +1,26 @@
 package com.example.alan.liveable_city_beta;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.location.Location;
-import android.location.LocationListener;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
 
-
-
-import java.text.DateFormat;
-import java.util.Date;
-
-import java.util.List;
 
 /**
  * Created by Alan on 19/5/2015.
  */
-public class SensorListenerService extends Service implements SensorEventListener, LocationListener {
+public class SensorListenerService extends Service implements  GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, ResultCallback<Status> {
 
     int mStartMode;       // indicates how to behave if the service is killed
     IBinder mBinder;      // interface for clients that bind
@@ -33,40 +29,45 @@ public class SensorListenerService extends Service implements SensorEventListene
     private SensorManager sensorManager = null;
     private Sensor sensor = null;
 
+
+    /**
+     * Used when requesting or removing activity detection updates.
+     */
+    private PendingIntent mActivityDetectionPendingIntent;
     /**
      * Provides the entry point to Google Play services.
      */
-    protected GoogleApiClient mGoogleApiClient;
+    //protected GoogleApiClient mGoogleApiClient;
     protected static final String TAG = "location-updates-sample";
+
+
+    /*google activity detection*/
+    protected GoogleApiClient mGoogleApiClient;
+
+    /**
+     * Builds a GoogleApiClient. Uses the {@code #addApi} method to request the
+     * ActivityRecognition API.
+     */
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(ActivityRecognition.API)
+                .build();
+    }
+
 
     @Override
     public void onCreate() {
+        buildGoogleApiClient();
         // The service is being created
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this, "sensor service starting", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient.connect();
 
 
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-
-        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        for (Sensor sensor : sensors)
-        {
-            sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
-        }
-
-
-      /*  sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
-        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
-        */
-
-
-
-        //stopSelf();
-        // The service is starting, due to a call to startService()
         return mStartMode;
     }
     @Override
@@ -87,81 +88,127 @@ public class SensorListenerService extends Service implements SensorEventListene
     @Override
     public void onDestroy() {
         Toast.makeText(this, "sensor service stop", Toast.LENGTH_SHORT).show();
+        mGoogleApiClient.disconnect();
+        removeActivityUpdates();
         // The service is no longer used and is being destroyed
     }
 
 
+    /**
+     * Runs when a GoogleApiClient object successfully connects.
+     */
+
     @Override
-    public void onSensorChanged(SensorEvent event) {
+    public void onConnected(Bundle bundle) {
+        Log.i(TAG, "Connected to GoogleApiClient");
+        requestActivityUpdates();
+    }
 
-        Sensor mySensor = event.sensor;
+    @Override
+    public void onConnectionSuspended(int i) {
 
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            Log.i("MyActivity", "ACCELEROMETER x = " + x+"y = "+y+"z = "+z);
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + connectionResult.getErrorCode());
+
+    }
+
+    @Override
+    public void onResult(Status status) {
+
+        if (status.isSuccess()) {
+
+            Log.e(TAG, "status is successful: " + status.getStatusMessage());
+        } else {
+            Log.e(TAG, "Error adding or removing activity detection: " + status.getStatusMessage());
         }
-        else if (mySensor.getType() == Sensor.TYPE_PROXIMITY) {
-            float x = event.values[0];
 
-            Log.i("MyActivity", "PROXIMITY x=" + x);
-        }
-        else if (mySensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-            Log.i("MyActivity", "MAGNETIC_FIELD x = " + x+"y = "+y+"z = "+z);
-        }
-
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
-
-
-    /*location call back*/
-
-    @Override
-    public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
-
-    protected void createLocationRequest() {
-        LocationRequest mLocationRequest = new LocationRequest();
-        mLocationRequest.setInterval(10000);
-        mLocationRequest.setFastestInterval(5000);
-        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     /**
-     * Builds a GoogleApiClient. Uses the {@code #addApi} method to request the
-     * LocationServices API.
+     * Receiver for intents sent by DetectedActivitiesIntentService via a sendBroadcast().
+     * Receives a list of one or more DetectedActivity objects associated with the current state of
+     * the device.
+
+    public class ActivityDetectionBroadcastReceiver extends BroadcastReceiver {
+        protected static final String TAG = "activity-detection-response-receiver";
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ArrayList<DetectedActivity> updatedActivities =
+                    intent.getParcelableArrayListExtra(Constants.ACTIVITY_EXTRA);
+
+           Log.i(TAG, "work");
+            //updateDetectedActivitiesList(updatedActivities);
+        }
+    }*/
+
+
+    /**
+     * Registers for activity recognition updates using
+     * {@link com.google.android.gms.location.ActivityRecognitionApi#requestActivityUpdates} which
+     * returns a {@link com.google.android.gms.common.api.PendingResult}. Since this activity
+     * implements the PendingResult interface, the activity itself receives the callback, and the
+     * code within {@code onResult} executes. Note: once {@code requestActivityUpdates()} completes
+     * successfully, the {@code DetectedActivitiesIntentService} starts receiving callbacks when
+     * activities are detected.
      */
-    protected synchronized void buildGoogleApiClient() {
-        Log.i(TAG, "Building GoogleApiClient");
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-        createLocationRequest();
+    public void requestActivityUpdates() {
+        if (!mGoogleApiClient.isConnected()) {
+            Log.i(TAG, "Unable to Connected to GoogleApiClient");
+            return;
+        }
+        ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(
+                mGoogleApiClient,
+                Constants.DETECTION_INTERVAL_IN_MILLISECONDS,
+                getActivityDetectionPendingIntent()
+        ).setResultCallback(this);
+        Log.i(TAG, "Connected to GoogleApiClient");
     }
+
+    /**
+     * Removes activity recognition updates using
+     * {@link com.google.android.gms.location.ActivityRecognitionApi#removeActivityUpdates} which
+     * returns a {@link com.google.android.gms.common.api.PendingResult}. Since this activity
+     * implements the PendingResult interface, the activity itself receives the callback, and the
+     * code within {@code onResult} executes. Note: once {@code removeActivityUpdates()} completes
+     * successfully, the {@code DetectedActivitiesIntentService} stops receiving callbacks about
+     * detected activities.
+     */
+    public void removeActivityUpdates() {
+        if (!mGoogleApiClient.isConnected()) {
+            Toast.makeText(this, getString(R.string.not_connected), Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // Remove all activity updates for the PendingIntent that was used to request activity
+        // updates.
+        ActivityRecognition.ActivityRecognitionApi.removeActivityUpdates(
+                mGoogleApiClient,
+                getActivityDetectionPendingIntent()
+        ).setResultCallback(this);
+    }
+
+    /**
+     * Gets a PendingIntent to be sent for each activity detection.
+     */
+    private PendingIntent getActivityDetectionPendingIntent() {
+        // Reuse the PendingIntent if we already have it.
+        if (mActivityDetectionPendingIntent != null) {
+
+            return mActivityDetectionPendingIntent;
+        }
+
+        Intent intent = new Intent(this, DetectedActivitiesIntentService.class);
+
+        // We use FLAG_UPDATE_CURRENT so that we get the same pending intent back when calling
+        // requestActivityUpdates() and removeActivityUpdates().
+        return PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+
 }
